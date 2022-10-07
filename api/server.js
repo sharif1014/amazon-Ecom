fba31447a95a817b7551af0a8d714743aea34f92
sharif1014 <sharifuliuk@gmail.com>
2022-10-07 14:59:46 +0600

    server added

---
+++ api/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+server.get("/", (req, res) => {
+    res.send('Welcome to node js tutorial');
+});
+server.get("/product/:id", (req, res) => {
+    res.send('Welcome to single product page');
+});
+server.listen(PORT, (req, res) => {
+    console.log(`server is running at ${PORT}`)
+});
